b56c020cdbeb839c65bcc43c184a1fae9e0bf1ae
Nathaniel Nutter <iam@nnutter.com>
2018-03-06 22:22:28 -0600

    Create init.sh

---
+++ init.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+
+git --git-dir ~/.dotfiles.git --work-tree ~ init
+git --git-dir ~/.dotfiles.git               config status.showUntrackedFiles no
+git --git-dir ~/.dotfiles.git               remote add -f origin https://github.com/nnutter/dotfiles.git
+git --git-dir ~/.dotfiles.git --work-tree ~ checkout -f master
+git --git-dir ~/.dotfiles.git               remote set-url origin git@github.com:nnutter/dotfiles.git
+
+git config --file ~/.config/git/config transfer.fsckObjects false
+git --git-dir ~/.dotfiles.git --work-tree ~ submodule update --init
+git config --file ~/.config/git/config transfer.fsckObjects true
